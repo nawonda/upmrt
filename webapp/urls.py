581325d353360d8a30cae46454b3aaaef67eb665
‘YUZ’ <jy1215jy@gmail.com>
2016-03-23 22:57:49 +0800

    basic view

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,4 +1,4 @@
-"""upmrt URL Configuration
+"""project1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
-from django.contrib import admin
+from . import views
 
-urlpatterns = [	
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('webapp.urls')),
+
+urlpatterns = [
+    url(r'^$', views.index,name='index'),
+    url(r'^contact/$', views.contact,name='contact'),
+
 ]
-
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from webapp.models import Post
 
-# Create your views here.
+
+def index(request):
+	return render(request, 'webapp/home.html')
+
+def contact(request):
+
+	postObjects = Post.objects.all().order_by("-date")[:25]	
+	return render(request, 'webapp/contact.html',{'info':postObjects})	
+	# return render(request, 'webapp/contact.html',{'info':['jerry','jy1215jy@gmail.com']})	
